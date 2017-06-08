fe3a915e7614cc559587238508d7ef855c4bec13
disne_y <bold.disney@gmail.com>
2017-06-08 15:39:45 +0300

    Add Route model and controller

---
+++ app/controllers/routes_controller.rb
@@ -0,0 +1,50 @@
+class RoutesController < ApplicationController
+  before_action :set_route, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @routes = Route.all
+  end
+
+  def show
+  end
+
+  def new
+    @route = Route.new
+  end
+
+  def edit
+  end
+
+  def create
+    @route = Route.new(route_params)
+
+    if @route.save
+      redirect_to @route, notice: 'Route was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @route.update(route_params)
+      redirect_to @route, notice: 'Route was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @train.destroy
+    redirect_to routes_url, notice: 'Route was successfully destroyed.'
+  end
+
+  private
+
+  def set_route
+    @route = Route.find(params[:id])
+  end
+
+  def route_params
+    params.require(:route).permit(:title)
+  end
+end
