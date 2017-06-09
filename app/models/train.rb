5d1b092f61b68ca0d4d963d372b39555351a266d
disne_y <bold.disney@gmail.com>
2017-06-09 14:15:13 +0300

    Train belongs to Route, Route has many trains.

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,5 +1,6 @@
 class Train < ApplicationRecord
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :route
 
   validates :number, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609104559) do
+ActiveRecord::Schema.define(version: 20170609111036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,9 @@ ActiveRecord::Schema.define(version: 20170609104559) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "current_station_id"
+    t.integer  "route_id"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id", using: :btree
+    t.index ["route_id"], name: "index_trains_on_route_id", using: :btree
   end
 
 end
