a4303d01692b74fee2e57ef349f6485365414dc8
disne_y <bold.disney@gmail.com>
2017-06-09 20:23:19 +0300

    Add pry-rails gem. Minor fix in RailwayStation's model

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,10 +1,8 @@
 class RailwayStation < ApplicationRecord
   has_many :trains
-  has_many :tickets
 
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
-  validates :title, presence: true, length: { in: 3..20 }
-  validates :title, uniqueness: true
+  validates :title, presence: true, length: { in: 3..20 }, uniqueness: true
 end
