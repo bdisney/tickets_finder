0b34d2ec3cbc322cdbbda2f2a84e94536c98f179
disne_y <bold.disney@gmail.com>
2017-06-09 15:36:56 +0300

    Station has many tickets

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,9 +1,10 @@
 class RailwayStation < ApplicationRecord
   has_many :trains
+  has_many :tickets
+
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
-  validates :title, presence: true,
-            length: { in: 3..20 }
+  validates :title, presence: true, length: { in: 3..20 }
   validates :title, uniqueness: true
 end
