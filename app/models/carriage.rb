a85cef9d4b9cf688b41a2c05d9759a4eba3ba457
disne_y <bold.disney@gmail.com>
2017-06-13 18:12:52 +0300

    Update Carriage model

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,7 +1,18 @@
 class Carriage < ApplicationRecord
   belongs_to :train
 
-  VARIATIONS = {economy: 'плацкарт', stateroom: 'купе'}.freeze
-  SEATS = {top_seats: 'верхние места', bottom_seats: 'нижние места'}.freeze
+  TYPES = {
+      EconomyCarriage:  'плацкарт',
+      CoupeCarriage:    'купе',
+      BusinessCarriage: 'бизнес',
+      SittingCarriage:  'сидячий'
+  }.freeze
 
+  SEATS = {
+      top_seats:        'верхние места',
+      bottom_seats:     'нижние места',
+      side_top_seats:   'верхние боковые места',
+      side_bottom_seats: 'нижние боковые места',
+      seats:            'сидячие места'
+  }.freeze
 end
