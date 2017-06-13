8dd1f82833dc2d89f763b052e7d6b67abb6b5177
disne_y <bold.disney@gmail.com>
2017-06-13 19:43:30 +0300

    Fix permitted params

--- app/models/business_carriage.rb
+++ app/models/business_carriage.rb
@@ -2,6 +2,6 @@ class BusinessCarriage < Carriage
   validates :bottom_seats, numericality: { only_integer: true, greater_than: 0 }
 
   def self.permitted_params
-    super.insert(-1, :top_seats, :bottom_seats)
+    super.insert(-1, :bottom_seats)
   end
 end
--- app/models/sitting_carriage.rb
+++ app/models/sitting_carriage.rb
@@ -2,6 +2,6 @@ class SittingCarriage < Carriage
   validates :seats, numericality: { only_integer: true, greater_than: 0 }
 
   def self.permitted_params
-    super.insert(-1, :top_seats, :bottom_seats)
+    super.insert(-1, :seats)
   end
 end
