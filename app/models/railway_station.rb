016d558cf3b33eed960fdc9ec937efbd67b8e3eb
disne_y <bold.disney@gmail.com>
2017-06-14 20:27:05 +0300

    Add update station position in route method

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -5,4 +5,13 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
 
   validates :title, presence: true, length: { in: 3..20 }, uniqueness: true
+
+  def relation_with(route)
+    @record ||= RailwayStationsRoute.find_by(route: route, railway_station: self)
+  end
+
+  def update_current_position(route, position)
+    current_position = relation_with(route)
+    current_position.update(position: position) if current_position
+  end
 end
