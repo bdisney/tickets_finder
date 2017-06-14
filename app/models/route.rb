807d64b4ba2024b5620a25ee869faa7362444300
disne_y <bold.disney@gmail.com>
2017-06-14 21:14:03 +0300

    Auto set route title

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,5 +3,12 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
 
+  before_validation :set_title
   validates :title, presence: true
+
+  private
+
+  def set_title
+    self.title = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
 end
