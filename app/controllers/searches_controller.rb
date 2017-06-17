fd6939d9955c248f680e9320df6d4c9b5de504b6
disne_y <bold.disney@gmail.com>
2017-06-17 17:23:37 +0300

    Implement search model and searches controller

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,16 @@
+class SearchesController < ApplicationController
+  def show
+  end
+
+  def create
+    @trains = Search.find_train(params[:departure_station], params[:arrival_station])
+
+    if @trains.present?
+      @departure_station = RailwayStation.find(params[:departure_station])
+      @arrival_station = RailwayStation.find(params[:arrival_station])
+      render :show
+    else
+      redirect_to search_path, notice: 'По вашему запросу ничего не найдено.'
+    end
+  end
+end
