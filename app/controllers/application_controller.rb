c0e8f78b923b6bd56df06846dbc1059f40bacb4c
disne_y <bold.disney@gmail.com>
2017-06-19 17:01:05 +0300

    Customize devise registration form, add devise permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name])
+  end
 end
