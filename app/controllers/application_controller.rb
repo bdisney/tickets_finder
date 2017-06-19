445f30752455e8d33519041d7d123c5f586d0bff
disne_y <bold.disney@gmail.com>
2017-06-19 20:08:02 +0300

    Add after sign_in path, skip authentication on search page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name])
   end
+
+  def after_sign_in_path_for(current_user)
+    current_user.admin? ? admin_routes_path : root_path
+  end
 end
