bae6e3ea1fedbe88e9269afb859cf618085cc6f8
disne_y <bold.disney@gmail.com>
2017-06-20 19:05:53 +0300

    Implement new ticket path helper

---
+++ app/helpers/searches_helper.rb
@@ -0,0 +1,10 @@
+module SearchesHelper
+  def ticket_path_helper(train)
+    params = {
+        train_id: train.id,
+        start_station_id: @departure_station,
+        last_station_id: @arrival_station
+    }
+    path = current_user.admin? ? new_admin_ticket_path(params) : new_ticket_path(params)
+  end
+end
