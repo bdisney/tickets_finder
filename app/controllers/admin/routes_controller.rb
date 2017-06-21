b37152d3de62c0f3670b443e5cab33ed9e7c018f
disne_y <bold.disney@gmail.com>
2017-06-21 16:35:30 +0300

    Translate controllers action notices

--- app/controllers/admin/routes_controller.rb
+++ app/controllers/admin/routes_controller.rb
@@ -19,7 +19,7 @@ class Admin::RoutesController < Admin::BaseController
     @route = Route.new(route_params)
 
     if @route.save
-      redirect_to admin_route_path(@route), notice: 'Route was successfully created.'
+      redirect_to admin_route_path(@route), notice: t('.notice')
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::RoutesController < Admin::BaseController
 
   def update
     if @route.update(route_params)
-      redirect_to admin_route_path(@route), notice: 'Route was successfully updated.'
+      redirect_to admin_route_path(@route), notice: t('.notice')
     else
       render :edit
     end
@@ -35,7 +35,7 @@ class Admin::RoutesController < Admin::BaseController
 
   def destroy
     @route.destroy
-    redirect_to admin_routes_path, notice: 'Route was successfully destroyed.'
+    redirect_to admin_routes_path, notice: t('.notice')
   end
 
   private
--- app/controllers/admin/trains_controller.rb
+++ app/controllers/admin/trains_controller.rb
@@ -19,7 +19,7 @@ class Admin::TrainsController < Admin::BaseController
     @train = Train.new(train_params)
 
     if @train.save
-      redirect_to admin_train_path(@train), notice: 'Train was successfully created.'
+      redirect_to admin_train_path(@train), notice: t('.notice')
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::TrainsController < Admin::BaseController
 
   def update
     if @train.update(train_params)
-      redirect_to admin_train_path(@train), notice: 'Train was successfully updated.'
+      redirect_to admin_train_path(@train), notice: t('.notice')
     else
       render :edit
     end
@@ -35,7 +35,7 @@ class Admin::TrainsController < Admin::BaseController
 
   def destroy
     @train.destroy
-    redirect_to admin_trains_path, notice: 'Train was successfully destroyed.'
+    redirect_to admin_trains_path, notice: t('.notice')
   end
 
   private
