06e25caab04245b40602af4af3c017e8ba0134c2
disne_y <bold.disney@gmail.com>
2017-06-23 13:53:45 +0300

    Render search form and search result via js

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,17 @@ class SearchesController < ApplicationController
   def create
     @trains = Search.find_train(params[:departure_station],
                                 params[:arrival_station])
-    if @trains.present?
-      @departure_station = RailwayStation.find(params[:departure_station])
-      @arrival_station   = RailwayStation.find(params[:arrival_station])
-      render :show
-    else
-      redirect_to search_path, notice: t('.warning')
+
+    respond_to do |format|
+      if @trains.present?
+        @departure_station = RailwayStation.find(params[:departure_station])
+        @arrival_station   = RailwayStation.find(params[:arrival_station])
+        format.js
+
+      else
+        format.html {render :show}
+        format.js
+      end
     end
   end
 end
